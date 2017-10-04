881279919c212e641500cd0d975585e862266e1f
Russell Barthel <russell.barthel@gmail.com>
2017-10-04 20:08:31 +0000

    users can paste img urls and they will display

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -20,7 +20,7 @@ class ChatBar extends Component {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder='Your Name: (Optional)' onBlur={ this.nameInput.bind(this) }/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyUp={ this.contentInput.bind(this) } />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER - To add an image paste the URL" onKeyUp={ this.contentInput.bind(this) } />
       </footer>
     )
   }
--- src/Message.jsx
+++ src/Message.jsx
@@ -3,13 +3,29 @@ import React, {Component} from 'react';
 class Message extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      img: '',
+      URLcheck: new RegExp('(\\S*.[jpg][png][gif])')
+    }
+  }
+
+  renderContent(content) {
+    let renderedContent = content;
+    if (renderedContent.match(this.state.URLcheck)) {
+      this.state.img = renderedContent.match(this.state.URLcheck)[0];
+      renderedContent = renderedContent.replace(this.state.URLcheck, '');
+    }
+    return renderedContent;
   }
 
   render() {
     return (
       <div className={ this.props.message.type === 'incomingMessage' ? 'message' : 'message system' } >
         <span className="message-username" style={{color: this.props.message.color}}>{ this.props.message.username }</span>
-        <span className="message-content">{ this.props.message.content }</span>
+        <span className="message-content">
+          { this.renderContent(this.props.message.content) }
+          <br /><img src={this.state.img} style={{'maxWidth': '60%'}} />
+        </span>
       </div>
     )
   }
